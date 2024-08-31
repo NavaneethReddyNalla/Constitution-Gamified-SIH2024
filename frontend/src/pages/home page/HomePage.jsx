c8059da8dba31e71f236e66bbf3b5a282505f2b0
Madhumitha <hattimadhumitha@gmail.com>
2024-08-31 14:39:34 +0530

    added video onto homepage

--- frontend/src/pages/home page/HomePage.jsx
+++ frontend/src/pages/home page/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import HomePageImage from "../../images/HomePageImage.jpg";
+import { Link } from "react-router-dom";
+//import HomePageImage from "../../images/HomePageImage.jpg";
+import book_zoom from '../../images/book_zoom.mp4'
 import "./HomePage.css";
+
 function HomePage() {
   return (
     <div className="home-page-main">
-      <img className="home-bg" src={HomePageImage} alt="background-image" />
-      <h1 className="home-heading">Start The Quiz!</h1>
-      <p className="home-subheading fs-4 lead text-center"> Select any Category to start the flash-card quiz</p>
+      <video className="home-bg-video" autoPlay loop muted>
+         <source src={book_zoom} type="video/mp4" /> 
+        Your browser does not support the video tag.
+      </video>
+      <h1 className="home-heading">Sansthaein Aur Samvidhan</h1>
+      <p className="home-subheading fs-4 lead text-center">"Play, Learn, Empower: Master the Constitution Through Fun and Games!"</p>
+      <Link to="/chatbot-vidhur" className="btn btn-primary me-3">Learn the Constitution </Link>
+      <Link to="/flash" className="btn btn-secondary">Test Your Knowledge</Link>
     </div>
   );
 }
